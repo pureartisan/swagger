e7d6129c07164db4f16c84aadfa49f2fd4b7eb22
Prageeth Silva <prageethsilva@gmail.com>
2020-05-11 11:13:26 +0200

    fix theme color

--- src/js/components/app-entry/index.js
+++ src/js/components/app-entry/index.js
@@ -2,29 +2,22 @@ import React from 'react';
 
 import { ThemeProvider } from '@material-ui/styles';
 import { createMuiTheme } from '@material-ui/core/styles';
-import useMediaQuery from '@material-ui/core/useMediaQuery';
 import green from '@material-ui/core/colors/green';
 
 import { HomePage } from 'src/js/components/home-page';
 
+const theme = createMuiTheme({
+  palette: {
+    type: 'light',
+    primary: green,
+  },
+});
 
-const AppEntry = () => {
-
-  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
-
-  const theme = createMuiTheme({
-    palette: {
-      type: prefersDarkMode ? 'dark' : 'light',
-      primary: green,
-    },
-  });
-
-  return (
-    <ThemeProvider theme={theme}>
-      <HomePage />
-    </ThemeProvider>
-  );
-}
+const AppEntry = () => (
+  <ThemeProvider theme={theme}>
+    <HomePage />
+  </ThemeProvider>
+);
 
 export {
   AppEntry
